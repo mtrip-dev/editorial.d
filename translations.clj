5902dd555ae7e4356e10ab21f916cefd205c8252
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2014-03-21 19:21:34 +0000

    to translate a keyword to its good target language

---
+++ translations.clj
@@ -0,0 +1,20 @@
+{:introduction {
+                "en" "introduction"
+                "fr" "introduction"
+                "pt" "introdução"
+                "it" "introduzione"
+                "es" "inicio"
+                "ru" "интродукция"
+                "ar" "مقدمة"
+                "ja" "前書き"
+                "zh" "绪言"
+                "ko" "입문"
+                "nl" "introductie"
+                "pl" "przedmowa"
+                "de" "einführung"}}
+
+
+
+
+
+
